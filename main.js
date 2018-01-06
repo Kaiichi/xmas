cf22b3257bcf2102a906c581f634fd51b4a82bba
Kaiichi <ugo.venture@gmail.com>
2018-01-06 10:17:28 +0100

    AddJS

---
+++ main.js
@@ -0,0 +1,12 @@
+function show_hidden(id)
+{
+    if(document.getElementById(id).style.visibility=="hidden")
+    {
+        document.getElementById(id).style.visibility="visible";
+    }
+    else
+    {
+        document.getElementById(id).style.visibility="hidden";
+    }
+    return true;
+}
