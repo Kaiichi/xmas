6186eaf041aa21edd9e05e040cfce9d580a1c524
Kaiichi <ugo.venture@gmail.com>
2018-01-08 20:36:48 +0100

    JSUpload1

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-function show_hidden(id)
-{
-    if(document.getElementById(id).style.visibility=="hidden")
-    {
-        document.getElementById(id).style.visibility="visible";
+function toggle_div(bouton, id) {
+ 
+      var div = document.getElementById(id);
+     
+      if(div.style.display=="none") {
+     
+        div.style.display = "block";
+     
+      } else {
+     
+        div.style.display = "none";
+     
+      }
+     
     }
-    else
-    {
-        document.getElementById(id).style.visibility="hidden";
-    }
-    return true;
-}
