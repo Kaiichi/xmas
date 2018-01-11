fba9b71a8410c186604f237a1565aa8801133aba
Kaiichi <ugo.venture@gmail.com>
2018-01-11 21:50:25 +0100

    PicturesFunctionT1

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 function toggle_div(bouton, id) {
- 
-      var div = document.getElementById(id);
-     
-      if(div.style.display=="none") {
-     
-        div.style.display = "block";
-     
-      } else {
-     
-        div.style.display = "none";
-     
-      }
+ 
+    var div = document.getElementById(id);
+   
+    if(div.style.display=="none") {
+   
+      div.style.display = "block";
+   
+    } else {
+   
+      div.style.display = "none";
+   
+    }
+   
+  }
+
+function pictures(bouton, id) {
+    var img = document.getElementById(id);
+
+    if (img.style.margin == "10% 30%").onclick {
+
+        img.style.margin = "50% 30%";
+    }
+    else {
+        img.style.margin = "10% 30%";
+    }
     return true;
-}
-function close(bouton ,id) {
-    document.getElementById(id).style.display=="none";
 }
